2fe98f42529e4025aaaf585b3c2f6a6bce2a9695
Antonio De Vivo <antoniod@bhblasted.com>
2025-02-28 15:24:33 +0100

    adding fastify

--- apps/daily-spending/src/main.ts
+++ apps/daily-spending/src/main.ts
@@ -1,18 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+
+  const globalPrefix = 'api';
+
+  await app.listen(3000, '0.0.0.0'); // Ascolta sulla porta 3000
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );

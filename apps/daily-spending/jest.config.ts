c7a3a42252794ab7d2b93b612647ab48a2e3b767
Antonio De Vivo <antoniod@bhblasted.com>
2025-02-28 15:09:38 +0100

    first commit

---
+++ apps/daily-spending/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'daily-spending',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/daily-spending',
+};
